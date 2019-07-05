5f0577877d390d9390944c95998b618012b30216
Michal Czeslaw Piotrowicz <memfagor@gmail.com>
2019-07-05 23:05:51 +0200

    Move functions from navalbattle unit to base unit
    Move functions:
    colorized_write()
    print_point()
    print_field()
    print_battlefield()
    from unit navalbattle to unit base.

--- base.pas
+++ base.pas
@@ -28,6 +28,80 @@ player = record
 
 players = array [0..1] of player;
 
+procedure colorized_write(txt : string; color : byte);
+procedure print_point(obj : point);
+procedure print_field(var obj : field; p_x, p_y : byte; hidden : point);
+procedure print_battlefield(var obj : players; p_x, p_y : byte);
+
 implementation
+
+procedure colorized_write(txt : string; color : byte);
+
+begin
+  textcolor(color);
+  write(txt);
+  textcolor(DEFAULT_TEXT_COLOR);
+end;
+
+procedure print_point(obj : point);
+
+begin
+  case obj of
+    empty : colorized_write('.',DEFAULT_TEXT_COLOR);
+    occupied : colorized_write('*',2);
+    hit : colorized_write('*',4);
+    miss : colorized_write('+',1);
+    marked : colorized_write('+',14);
+  end;
+end;
+
+procedure print_field(var obj : field; p_x, p_y : byte; hidden : point);
+
+var
+  indx_y : byte;
+  indx_x : byte;
+
+begin
+  gotoxy(p_x+2,p_y);
+  for indx_x := 0 to DEFAULT_BOARD_SIZE do write(indx_x+1:2);
+  gotoxy(p_x+2,p_y+1);
+  for indx_x := 0 to 19 do write('-');
+  for indx_y := 0 to DEFAULT_BOARD_SIZE do
+  begin
+    gotoxy(p_x,p_y+2+indx_y);
+    write(indx_y+1:2);
+    write('|');
+    for indx_x := 0 to DEFAULT_BOARD_SIZE do
+    begin
+      if obj[indx_x,indx_y] = hidden then print_point(empty) else print_point(obj[indx_x,indx_y]);
+      write(' ');
+    end;
+  end;
+end;
+
+procedure print_battlefield(var obj : players; p_x, p_y : byte);
+
+var
+  indx : byte;
+  effectivity : byte;
+
+begin
+  print_field(obj[0].bfield,p_x,p_y,empty);
+  print_field(obj[1].bfield,p_x+25,p_y,occupied);
+  for indx := 0 to 1 do
+  begin
+    if (obj[indx].hit = 0) and (obj[indx].miss = 0) then effectivity := 0 else effectivity := round((obj[indx].hit/(obj[indx].hit+obj[indx].miss))*100);
+    gotoxy((p_x+2+indx*25),p_y+16);
+    write('Hit :');
+    write(obj[indx].hit:3);
+    gotoxy((p_x+2+indx*25),p_y+17);
+    write('Miss :');
+    write(obj[indx].miss:3);
+    gotoxy((p_x+2+indx*25),p_y+18);
+    write('Efectivity :');
+    write(effectivity:3);
+    write('%');
+  end;
+end;
 
 end.
